f86b790ccd276e6f606c64e8173b446291b15f74
Matt Hayes <matt@mysterycommand.com>
2015-08-24 20:48:09 -0700

    Add more linting stuff and an initial App component.

---
+++ lib/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react/addons';
+
+export default class App {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>App</div>
+        );
+    }
+};
+
+App.displayName = 'App';
